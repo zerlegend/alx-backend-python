cf297996f307740491a127728df67be5b918a2f8
zerlegend <zerlegend@gmail.com>
2023-10-05 19:36:14 +0000

    updated files

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,11 +3,9 @@
 '''
 from typing import Any, Mapping, Union, TypeVar
 
-
 T = TypeVar('T')
 Res = Union[Any, T]
 Def = Union[T, None]
-
 
 def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
     '''Retrieves a value from a dict using a given key.
--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -3,7 +3,6 @@
 '''
 from typing import List, Tuple
 
-
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
     '''Creates multiple copies of items in a tuple.
     '''
@@ -13,9 +12,6 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List:
     ]
     return zoomed_in
 
-
 array = (12, 72, 91)
-
 zoom_2x = zoom_array(array)
-
 zoom_3x = zoom_array(array, 3)
