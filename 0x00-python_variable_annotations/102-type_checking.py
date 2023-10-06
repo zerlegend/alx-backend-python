835ade579349ffc5c31005d814aa1a4eb6fa7860
zerlegend <zerlegend@gmail.com>
2023-10-06 04:26:35 +0000

    updated advanced tasks

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -3,6 +3,7 @@
 '''
 from typing import List, Tuple
 
+
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
     '''Creates multiple copies of items in a tuple.
     '''
@@ -12,6 +13,9 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List:
     ]
     return zoomed_in
 
+
 array = (12, 72, 91)
+
 zoom_2x = zoom_array(array)
+
 zoom_3x = zoom_array(array, 3)
